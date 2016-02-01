871a4551de4cb26176515e947d3a7810f64e7b67
Luis Rodrigo <luisrodrigo@luiss-macbook-pro.local>
2016-01-31 18:18:29 -0600

    Initial Commit

---
+++ Prueba Gif/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prueba Gif
+//
+//  Created by Luis Rodrigo on 1/31/16.
+//  Copyright © 2016 Luis Rodrigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prueba Gif/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prueba Gif
+//
+//  Created by Luis Rodrigo on 1/31/16.
+//  Copyright © 2016 Luis Rodrigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
